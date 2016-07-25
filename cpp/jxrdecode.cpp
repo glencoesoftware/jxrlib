aa6607c500bc639c0ec9ad989af5884bfc682bf2
jballanc <jballanc@glencoesoftware.com>
2016-07-25 03:56:05 -0400

    Add a simple test of C++ wrappers
    The `jxrdecode` app is a direct port of JXRLib's `JxrDecApp.c`, minus
    argument handling. This exists simply as a means of quickly validating
    the C++ wrappers are functioning as expected.The `jxrdecode` app is a
    direct port of JXRLib's `JxrDecApp.c`, minus argument handling. This
    exists simply as a means of quickly validating the C++ wrappers are
    functioning as expected.

---
+++ cpp/jxrdecode.cpp
@@ -0,0 +1,63 @@
+/*
+ * #%L
+ * Copyright (C) 2016 Glencoe Software, Inc. All rights reserved.
+ * %%
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * #%L
+ */
+
+#include <iostream>
+#include <string>
+
+#include "windowsmediaphoto.h"
+
+#include "CodecFactory.hpp"
+#include "Factory.hpp"
+#include "FormatConverter.hpp"
+#include "FormatError.hpp"
+#include "ImageDecoder.hpp"
+
+
+using namespace jxrlib;
+
+int main(int argc, char* argv[]) {
+  try {
+    Factory factory;
+    CodecFactory codecFactory;
+
+    std::string inputFile = argv[1];
+    ImageDecoder decoder = codecFactory.decoderFromFile(inputFile);
+    std::cout << "Opened decoder for file: " << inputFile << std::endl;
+
+    uint32_t frameCount = decoder.getFrameCount();
+    std::cout << "Found " << frameCount << " frames" << std::endl;
+
+    std::string outputFile = argv[2];
+    std::string extension = outputFile.substr(outputFile.find_last_of(".") + 1);
+
+    for(int i = 0 ; ; ++i) {
+        FormatConverter converter = codecFactory.createFormatConverter(decoder,
+                                                                       extension);
+        std::cout << "Created format converter for extension: " << extension << std::endl;
+    }
+
+    std::cout << "Hello, world..." << std::endl;
+    return 0;
+  }
+  catch (const FormatError &e) {
+    std::cout << "*** ERROR: Unsupported format in JPEG XR ***";
+    return WMP_errUnsupportedFormat;
+  }
+}
