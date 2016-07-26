e704899771d47f2f6c70c6d96264fc250386a859
jballanc <jballanc@glencoesoftware.com>
2016-07-26 09:36:06 -0400

    Add a simple resolution class

---
+++ cpp/lib/Resolution.hpp
@@ -0,0 +1,35 @@
+/*
+ * #%L
+ * Copyright (C) 2016 Glencoe Software, Inc. All rights reserved.
+ * %%
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * #%L
+ */
+
+#ifndef _Resolution
+#define _Resolution
+
+namespace jxrlib {
+
+  struct Resolution {
+    float X;
+    float Y;
+    Resolution() : X(0.0), Y(0.0) {}
+    Resolution(float X, float Y) : X(X), Y(Y) {}
+  };
+
+} // namespace jxrlib
+
+#endif // _Resolution
