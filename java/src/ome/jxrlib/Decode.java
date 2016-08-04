9be712ac1618263623cd050d11a092ad37f4b600
jballanc <jballanc@glencoesoftware.com>
2016-08-04 15:39:59 -0400

    Remove the try/catch block from Decode
    Since the last commit removed exception translation, there are now no
    methods that will throw a Java exception, so this block is unnecessary.

--- java/src/ome/jxrlib/Decode.java
+++ java/src/ome/jxrlib/Decode.java
@@ -25,37 +25,32 @@ public class Decode {
     }
 
     public static void main(String args[]) {
-        try {
-                Factory factory = new Factory();
-                CodecFactory codecFactory = new CodecFactory();
+        Factory factory = new Factory();
+        CodecFactory codecFactory = new CodecFactory();
 
-                String inputFile = args[0];
-                ImageDecoder decoder = codecFactory.decoderFromFile(inputFile);
-                System.out.println("Opened decoder for file: " + inputFile);
+        String inputFile = args[0];
+        ImageDecoder decoder = codecFactory.decoderFromFile(inputFile);
+        System.out.println("Opened decoder for file: " + inputFile);
 
-                long frameCount = decoder.getFrameCount();
-                System.out.println("Found " + frameCount + " frame" + (frameCount > 1 ? "s" : ""));
+        long frameCount = decoder.getFrameCount();
+        System.out.println("Found " + frameCount + " frame" + (frameCount > 1 ? "s" : ""));
 
-                String outputFile = args[1];
-                String extension = outputFile.substring(outputFile.lastIndexOf('.') + 1);
+        String outputFile = args[1];
+        String extension = outputFile.substring(outputFile.lastIndexOf('.') + 1);
 
-                for (long i = 0 ; i < frameCount ; i++) {
-                    decoder.selectFrame(i);
-                    FormatConverter converter = codecFactory.createFormatConverter(decoder, extension);
-                    System.out.println("Created format converter for extension: " + extension);
-                    Stream outputStream = factory.createStreamFromFilename(outputFile);
-                    System.out.println("Created output stream for file: " + outputFile);
-                    ImageEncoder encoder = new ImageEncoder(outputStream, "." + extension);
-                    System.out.println("Created image encoder");
-                    encoder.initializeWithDecoder(decoder);
-                    encoder.writeSource(converter);
-                    encoder.close();
-                }
-                decoder.close();
-            }
-        catch (FormatError e) {
-            System.out.println("*** ERROR: Unsupported format in JPEG XR ***\n" + e.getMessage());
+        for (long i = 0 ; i < frameCount ; i++) {
+            decoder.selectFrame(i);
+            FormatConverter converter = codecFactory.createFormatConverter(decoder, extension);
+            System.out.println("Created format converter for extension: " + extension);
+            Stream outputStream = factory.createStreamFromFilename(outputFile);
+            System.out.println("Created output stream for file: " + outputFile);
+            ImageEncoder encoder = new ImageEncoder(outputStream, "." + extension);
+            System.out.println("Created image encoder");
+            encoder.initializeWithDecoder(decoder);
+            encoder.writeSource(converter);
+            encoder.close();
         }
+        decoder.close();
     }
 
 }
