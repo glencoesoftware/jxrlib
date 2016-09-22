d74de033f4ff83fa240049020a1bd4244823d628
Josh Ballanco <jballanc@gmail.com>
2016-09-22 17:28:03 +0300

    Add memory-based decoding to the Decode class
    This adds the ability to read from a file to bytes in memory, as well as
    decoding bytes in memory to pixels in memory directly. The `main`
    function is also updated to enable exercising these new capabilities.

--- java/src/ome/jxrlib/Decode.java
+++ java/src/ome/jxrlib/Decode.java
@@ -18,39 +18,115 @@
 
 package ome.jxrlib;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
 public class Decode {
 
     static {
         System.loadLibrary("jxrjava");
     }
 
-    public static void main(String args[]) {
-        Factory factory = new Factory();
-        CodecFactory codecFactory = new CodecFactory();
+    private static final Factory factory = new Factory();
+    private static final CodecFactory codecFactory = new CodecFactory();
 
-        String inputFile = args[0];
-        ImageDecoder decoder = codecFactory.decoderFromFile(inputFile);
-        System.out.println("Opened decoder for file: " + inputFile);
+    private final File inputFile;
+    private final byte data[];
+    private final ImageDecoder decoder;
+    private final long frameCount;
 
-        long frameCount = decoder.getFrameCount();
-        System.out.println("Found " + frameCount + " frame" + (frameCount > 1 ? "s" : ""));
+    public Decode(File inputFile) {
+        this.inputFile = inputFile;
+        this.data = null;
+        decoder = codecFactory.decoderFromFile(inputFile);
+        frameCount = decoder.getFrameCount();
+    }
 
-        String outputFile = args[1];
-        String extension = outputFile.substring(outputFile.lastIndexOf('.') + 1);
+    public Decode(byte data[]) {
+        this.inputFile = null;
+        this.data = data;
+        decoder = codecFactory.decoderFromBytes(data);
+        frameCount = decoder.getFrameCount();
+    }
+
+    protected void finalize() throws Throwable {
+        if (decoder != null) {
+            decoder.close();
+        }
+    }
+
+    public byte[] toBytes() {
+        ByteArrayOutputStream decodedBytes = new ByteArrayOutputStream();
+        for (long i = 0 ; i < frameCount ; i++) {
+            decoder.selectFrame(i);
+            ImageData data = decoder.getRawBytes();
+            for (int j = 0 ; j < data.size() ; j++) {
+                decodedBytes.write(data.get(j));
+            }
+        }
+        return decodedBytes.toByteArray();
+    }
+
+    public void toFile(File outputFile) {
+        String fileName = outputFile.getName();
+        String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
 
         for (long i = 0 ; i < frameCount ; i++) {
             decoder.selectFrame(i);
             FormatConverter converter = codecFactory.createFormatConverter(decoder, extension);
-            System.out.println("Created format converter for extension: " + extension);
-            Stream outputStream = factory.createStreamFromFilename(outputFile);
-            System.out.println("Created output stream for file: " + outputFile);
+            System.err.println("Created format converter for extension: " + extension);
+            Stream outputStream = factory.createStreamFromFilename(outputFile.getAbsolutePath());
+            System.err.println("Created output stream for file: " + fileName);
             ImageEncoder encoder = new ImageEncoder(outputStream, "." + extension);
-            System.out.println("Created image encoder");
+            System.err.println("Created image encoder");
             encoder.initializeWithDecoder(decoder);
             encoder.writeSource(converter);
             encoder.close();
         }
-        decoder.close();
+    }
+
+    public static void printBytes(byte imageBytes[]) {
+        // TODO: Better output formatting...
+        System.err.println("Decoded bytes:\n\n" + imageBytes + "\n\n");
+    }
+
+    public static void main(String args[]) {
+        Decode decode;
+
+        if (args.length == 0) {
+            ByteArrayOutputStream readData = new ByteArrayOutputStream();
+            byte[] buffer = new byte[32 * 1024];
+
+            int bytesRead;
+            try {
+                while ((bytesRead = System.in.read(buffer)) > 0) {
+                    readData.write(buffer, 0, bytesRead);
+                }
+                byte[] bytes = readData.toByteArray();
+
+                decode = new Decode(bytes);
+                System.err.println("Opened decoder for bytes...");
+                byte[] imageBytes = decode.toBytes();
+                printBytes(imageBytes);
+            } catch (IOException e) {
+                System.err.println("Problem parsing input data! " + e.getMessage());
+            }
+        } else {
+            File inputFile = new File(args[0]);
+
+            decode = new Decode(inputFile);
+            System.err.println("Opened decoder for file: " + inputFile);
+            if (args.length == 1) {
+                byte[] imageBytes = decode.toBytes();
+                printBytes(imageBytes);
+            } else if (args.length == 2) {
+                decode.toFile(new File(args[1]));
+            } else {
+                System.err.println("INVALID DECODE COMMAND");
+            }
+        }
     }
 
 }
